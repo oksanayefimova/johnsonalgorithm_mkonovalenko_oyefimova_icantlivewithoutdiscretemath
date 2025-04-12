5962c6c3c62e5347f9cba24abec50537a07fc6cf
oyefimova <oyefimova@kse.org.ua>
2025-04-12 15:24:48 +0300

    done graph generator

---
+++ AlgorithmImplementation/Program.cs
@@ -0,0 +1,9 @@
+﻿using icantlivewithoutdiscretemath;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(GraphGenerator.Generate(4, 0.1, true).AdjacencyList);
+    }
+}
